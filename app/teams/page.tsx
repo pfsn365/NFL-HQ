5c7d77d1ab0de6151805e04a3368cc7f17570e7f
James7599 <jfragoza@pfsn365.com>
2026-01-08 13:54:03 -0800

    Reduce hero section bottom padding to bring content up closer

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -193,7 +193,7 @@ export default function TeamsPage() {
       {/* Main Content */}
       <main className="flex-1 lg:ml-64 min-w-0 lg:pt-0 pt-14">
           {/* Header */}
-          <div className="bg-[#0050A0] text-white pt-[57px] lg:pt-0 pb-8 lg:pb-10">
+          <div className="bg-[#0050A0] text-white pt-[57px] lg:pt-0 pb-6 lg:pb-8">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 pt-8 lg:pt-10">
               <h1 className="text-3xl md:text-4xl lg:text-5xl font-black text-white mb-3 tracking-tight">
                 NFL Teams
